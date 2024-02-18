93dcc229bf2b4291db88a1370e8aca2a38d73d2d
Vitalii <vetalikradko8@gmail.com>
2024-02-18 16:05:01 +0100

    ..

--- Lab_1/task2.py
+++ Lab_1/task2.py
@@ -1,2 +1,13 @@
-for x in range (0,21,2):
-    print(x)
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+
+if 1 <= a <= 100 and 1 <= b <= 100:
+    if a > b:
+        X = a / b - 1
+    elif a == b:
+        X = -25
+    else:
+        X = (a**3 - 5) / a
+    print(f"Value of X: {X}")
+else:
+    print("Values for a and b must be between 1 and 100 and positive.")
--- Lab_1/task3.py
+++ Lab_1/task3.py
@@ -2,7 +2,7 @@ n = int(input("Введіть кількість чисел: "))
 number = -1
 while (n < 1 or n > 9):
     n = int(input("Введіть ще раз кількість чисел: "))
-for i in range(1, n + 1):
+for i i range(1, n + 1):
     print("")
     number += 1
     for j in range(n, number, -1):
